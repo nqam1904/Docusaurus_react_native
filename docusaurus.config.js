0c59513acf27a48a9231cf899c25135c26353a34
MinhNQA <nghiemminh1904@gmail.com>
2021-11-11 22:12:37 +0700

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,106 +6,115 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-   title: "Dev Mobile",
-   tagline: "Code xịn hơn mỗi ngày",
-   url: "https://your-docusaurus-test-site.com",
-   baseUrl: "/",
-   onBrokenLinks: "throw",
-   onBrokenMarkdownLinks: "warn",
-   favicon: "img/favicon.ico",
-   organizationName: "facebook", // Usually your GitHub org/user name.
-   projectName: "docusaurus", // Usually your repo name.
+  title: "Dev Mobile",
+  tagline: "Code xịn hơn mỗi ngày",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
-   presets: [
-      [
-         "@docusaurus/preset-classic",
-         /** @type {import('@docusaurus/preset-classic').Options} */
-         ({
-            docs: {
-               sidebarPath: require.resolve("./sidebars.js"),
-               // Please change this to your repo.
-               editUrl:
-                  "https://github.com/facebook/docusaurus/edit/main/website/",
-            },
-            blog: {
-               showReadingTime: true,
-               // Please change this to your repo.
-               editUrl:
-                  "https://github.com/facebook/docusaurus/edit/main/website/blog/",
-            },
-            theme: {
-               customCss: require.resolve("./src/css/custom.css"),
-            },
-         }),
-      ],
-   ],
-
-   themeConfig:
-      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-         navbar: {
-            title: "DEV MOBILE",
-            logo: {
-               alt: "My Site Logo",
-               src: "img/logo.svg",
-            },
-            items: [
-               {
-                  type: "doc",
-                  docId: "intro",
-                  position: "left",
-                  label: "Docs",
-               },
-               { to: "/blog", label: "Blog", position: "left" },
-               { to: "/help", label: "Help", position: "left" },
-            ],
-         },
-         footer: {
-            style: "dark",
-            links: [
-               {
-                  title: "Docs",
-                  items: [
-                     {
-                        label: "Tutorial",
-                        to: "/docs/intro",
-                     },
-                  ],
-               },
-               {
-                  title: "Kết nối",
-                  items: [
-                     {
-                        label: "Facebook cá nhân",
-                        href: "https://www.facebook.com/quocminh1904/",
-                     },
-                     {
-                        label: "Discord",
-                        href: "https://discord.gg/bchYjuhz",
-                     },
-                  ],
-               },
-               {
-                  title: "More",
-                  items: [
-                     {
-                        label: "Blog",
-                        to: "/blog",
-                     },
-                     {
-                        label: "GitHub",
-                        href: "https://github.com/nqam1904",
-                     },
-                  ],
-               },
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} Dev Mobile. Built with Docusaurus.`,
-         },
-         prism: {
-            theme: lightCodeTheme,
-            darkTheme: darkCodeTheme,
-         },
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/main/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
       }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "DEV MOBILE",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            to: "/components",
+            label: "Components",
+            docId: "intro",
+            position: "left",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          { to: "/help", label: "Help", position: "left" },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+              {
+                label: "Components",
+                to: "/components/intro",
+              },
+            ],
+          },
+          {
+            title: "Kết nối",
+            items: [
+              {
+                label: "Facebook cá nhân",
+                href: "https://www.facebook.com/quocminh1904/",
+              },
+              {
+                label: "Discord",
+                href: "https://discord.gg/bchYjuhz",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/nqam1904",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Dev Mobile. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
